7156610e3905484ee9a6a088b7e70e9a886ca290
TestAndunR <andunranmal@gmail.com>
2018-10-23 17:29:51 +0530

    hh

--- addNewProject/lambda.js
+++ addNewProject/lambda.js
@@ -1,8 +1,21 @@
 let AWS = require('aws-sdk');
+let SL_AWS = require('slappforge-sdk-aws');
+let connectionManager = require('./ConnectionManager');
+const rds = new SL_AWS.RDS(connectionManager);
 console.log("asasas")
-exports.handler = function(event, context, callback) {
-    
+
+exports.handler = function (event, context, callback) {
+
+
+    // You must always end/destroy the DB connection after it's used
+    rds.beginTransaction({
+        instanceIdentifier: 'bbb'
+    }, function (error, connection) {
+        if (error) {
+            throw error;
+        }
+    });
 
     console.log("User test for vcs state in git commit");
-    callback(null,'Successfully executed');
+    callback(null, 'Successfully executed');
 }
